98a19eb455753d43ec7865fa5aebca87dc17b3b5
Nicholas <romitinicholas@gmail.com>
2021-11-17 18:29:03 +0100

    colore degli square

--- script.js
+++ script.js
@@ -22,16 +22,34 @@ play.addEventListener("click", function () {
         container.innerHTML = items;
         const square = document.querySelectorAll(".square");
         console.log(square);
+        square.forEach((square) => {
+            square.addEventListener("click", () => {
+                square.classList.add("active");
+            })
+        })
     } else if (livello == "Hard") {
         for(let i = 1; i <= 81; i++) {
             items += `<div class="square-hard"></div>`;
         }
         container.innerHTML = items;
+        const squareHard = document.querySelectorAll(".square-hard");
+        console.log(squareHard);
+        squareHard.forEach((squareHard) => {
+            squareHard.addEventListener("click", () => {
+                squareHard.classList.add("active");
+            })
+        })
     } else {
         for(let i = 1; i <= 49; i++) {
             items += `<div class="square-crazy"></div>`;
         }
         container.innerHTML = items;
+        const squareCrazy = document.querySelectorAll(".square-crazy");
+        console.log(squareCrazy);
+        squareCrazy.forEach((squareCrazy) => {
+            squareCrazy.addEventListener("click", () => {
+                squareCrazy.classList.add("active");
+            })
+        })
     }
 })
-
